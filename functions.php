44a43c6666a14837326ea0fcaaa892856e1ca7ba
JR Tashjian <jrtashjian@gmail.com>
2018-12-05 16:48:21 -0500

    Pull in scss styles from TwentyNineteen for easier variable overrides.

--- functions.php
+++ functions.php
@@ -8,12 +8,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Add our editor style.
+ *
+ * @return void
+ */
 function nineteeneightyfive_setup() {
-	// Enqueue editor styles.
 	add_editor_style( 'style-editor.css' );
 }
 add_action( 'after_setup_theme', 'nineteeneightyfive_setup' );
 
+/**
+ * Override the parent theme's Comment's Walker class.
+ *
+ * @param array $arguments
+ *
+ * @return array
+ */
 function nineteeneightyfive_wp_list_comments_args( $arguments ) {
 	require get_stylesheet_directory() . '/classes/class-nineteeneightyfive-walker-comment.php';
 	$arguments['walker'] = new NineteenEightyFive_Walker_Comment();
@@ -21,17 +32,23 @@ function nineteeneightyfive_wp_list_comments_args( $arguments ) {
 }
 add_filter( 'wp_list_comments_args', 'nineteeneightyfive_wp_list_comments_args' );
 
+/**
+ * Enqueue our styles.
+ *
+ * @return void
+ */
 function nineteeneightyfive_enqueue_styles() {
-	$parent_style = 'twentynineteen';
-
-	// Enqueue parent theme's stylesheet.
-	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-
-	// Enqueue child theme's stylesheet.
-	wp_enqueue_style( 'nineteen-eighty-five', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+	wp_enqueue_style( 'nineteeneightyfive', get_stylesheet_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'nineteeneightyfive_enqueue_styles' );
 
+/**
+ * Override our comment form submit button so we can apply NES.css styles properly.
+ *
+ * @param array $defaults
+ *
+ * @return array
+ */
 function nineteeneightyfive_comment_form_defaults( $defaults ) {
 	$defaults['submit_button'] = '<button name="%1$s" type="submit" id="%2$s" class="%3$s btn is-primary">%4$s</button>';
 	return $defaults;
